a188b1b27798f4c9ce5e6c134bbc5e14a5cf47a9
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-07-21 23:19:25 +0200

    added smooth scroll to anchor links

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 import './sass/index.scss';
+
+import './js/modal';
---
+++ src/js/modal.js
@@ -0,0 +1,39 @@
+const openModalBtn = document.querySelector('[data-menu-open');
+const backdrop = document.querySelector('[data-modal');
+const closeBtn = document.querySelector('[data-modal-close]');
+const menuItemsRefs = document.querySelectorAll('[data-menu-item]');
+
+openModalBtn.addEventListener('click', onModalOpen);
+backdrop.addEventListener('click', onBackdropClick);
+closeBtn.addEventListener('click', onCloseBtnClick);
+
+function onModalOpen() {
+  backdrop.classList.remove('is-hidden');
+  document.body.style.overflow = 'hidden';
+
+  window.addEventListener('keydown', onEscClick);
+}
+
+function onEscClick(e) {
+  if (e.code === 'Escape') {
+    onCloseBtnClick();
+  }
+}
+
+function onBackdropClick(e) {
+  if (e.target === backdrop) {
+    backdrop.classList.add('is-hidden');
+    document.body.style.overflow = 'scroll';
+  }
+}
+
+function onCloseBtnClick() {
+  backdrop.classList.add('is-hidden');
+  document.body.style.overflow = 'scroll';
+
+  window.removeEventListener('keydown', onEscClick);
+}
+
+menuItemsRefs.forEach(item => {
+  item.addEventListener('click', onCloseBtnClick);
+});
